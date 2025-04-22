dcac4ac892a5cf02655f8844a023c705f96f5936
Tosbaba1 <tosan.agbeyegbe@gmail.com>
2025-04-22 21:05:10 +0000

    Full Dice Roll Game with Rails

--- app/controllers/diceroll_controller.rb
+++ app/controllers/diceroll_controller.rb
@@ -4,12 +4,53 @@ class DicerollController < ApplicationController
     render({ :template => "game_templates/home" })
   end
 
-  def 2d6
+  def d26
     @rolls = []
 
     2.times do
      die = rand(1..6)
 
     @rolls.push(die)
+    end
+
+  render ({ :template => "game_templates/d26" })
   end
+
+  def d210
+    @rolls = []
+
+    2.times do
+     die = rand(1..10)
+
+    @rolls.push(die)
+    end
+
+  render ({ :template => "game_templates/d210" })
+  
+  end
+
+  def d54
+    @rolls = []
+
+    5.times do
+     die = rand(1..4)
+
+    @rolls.push(die)
+    end
+
+  render ({ :template => "game_templates/d54" })
+  end
+
+  def d120
+    @rolls = []
+
+    1.times do
+     die = rand(1..20)
+
+    @rolls.push(die)
+    end
+
+  render ({ :template => "game_templates/d120" })
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
   get("/", { :controller => "diceroll", :action => "home"})
   
-  get("/dice/2/6", { :controller => "diceroll", :action => "2d6"})
+  get("/dice/2/6", { :controller => "diceroll", :action => "d26"})
   #controller is a class while action is the method we want rails to run
 
-  get("/dice/2/10", { :controller => "diceroll", :action => "2d10"})
+  get("/dice/2/10", { :controller => "diceroll", :action => "d210"})
 
-  get("/dice/1/20", { :controller => "diceroll", :action => "1d20"})
+  get("/dice/1/20", { :controller => "diceroll", :action => "d120"})
 
-   get("/dice/5/4", { :controller => "diceroll", :action => "5d4"})
-
-  # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
-
-  # get "/your_first_screen" => "pages#first"
+  get("/dice/5/4", { :controller => "diceroll", :action => "d54"})
   
 end
