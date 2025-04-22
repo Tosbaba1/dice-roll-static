7b6f2e5315f18b13d27b40fae64d1714c4c69d7b
Tosbaba1 <tosan.agbeyegbe@gmail.com>
2025-04-22 20:42:38 +0000

    Added home.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "diceroll", :action => "home"})
+  
+  get("/dice/2/6", { :controller => "diceroll", :action => "2d6"})
+  #controller is a class while action is the method we want rails to run
+
+  get("/dice/2/10", { :controller => "diceroll", :action => "2d10"})
+
+  get("/dice/1/20", { :controller => "diceroll", :action => "1d20"})
+
+   get("/dice/5/4", { :controller => "diceroll", :action => "5d4"})
 
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
 
